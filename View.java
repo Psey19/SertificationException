b2f0f4c30c4fe368bb617ad69bb45ce9823fe6e4
Psey19 <153425698+Psey19@users.noreply.github.com>
2024-06-21 17:52:27 +0500

    Create View.java

---
+++ View.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class View {
+    public String enterData(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите строку с данными через пробел\n" +
+                "Фамилия, Имя, Отчество, Дата Рождения (dd.MM.yyyy), " +
+                "Номер Телефона(только цифры), Пол (m/f):");
+        String data = sc.nextLine();
+        return data;
+    }
+}
